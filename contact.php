b05a857234af89d22933777382b9380bab045dc7
ArmandoGuerre <84296808+ArmandoGuerre@users.noreply.github.com>
2021-12-09 09:55:20 -0500

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Guerrero's Bakery Pitch</title>
+    <link rel="stylesheet" href="fpp.css">
+    <script src="fpp.js" defer></script>
+</head>
+<body>
+    <nav>
+		<h1>Guerrero's Bakery</h1>
+        <b>
+            <div class="menuIcon">
+				<div class="bar"></div> 
+				<div class="bar"></div> 
+				<div class="bar"></div>
+			</div>
+		</b>
+	</nav>
+    <div class="menu">
+        <h1><a href="index.html">Home</a></h1>&nbsp;
+        <h1><a href="about.html">About</a></h1> &nbsp;
+        <h1><a href="catering.html">Catering</a></h1> &nbsp;
+    </div>
+    <div id="wrapper">
+        <!--<div>
+            <img src="pan.jpg" 
+            alt="Rocky coast line" 
+            width="70%" 
+            height="350">
+        </div> -->	
+        <br><header>
+            <h1 class="extra">About Us</h1>
+        </header><br>
+        
+        <main>
+        <div id="content">
+	<h2>We will be contacting you soon!</h2>
+	<h3>Here is the information you entered:</h3><br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<form action="#">
+<input type="button" value = "Back" onclick="javascript:history.go(-1)" />
+</form>
+</div>
+</div>
+        </main>
+    </div>
+    <footer>
+        Copyright &copy; Guerrero's Bakery Pitch<br>
+        <a href="mailto:joguerre@iu.edu">joguerre@iu.edu</a>
+    </footer>
+    </body>
+</html>
